e1ca2a9020afd5968fcd5cc95e50fd6571b60b43
Roger Dunn <rogerdunn@Rogers-iMac.local>
2019-05-15 17:45:46 -0400

    second commit

--- colorGame.js
+++ colorGame.js
@@ -1,17 +1,25 @@
 
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)",
-]
+var colors = generateRandomColors(6)
+
+// [
+// 	"rgb(255, 0, 0)",
+// 	"rgb(255, 255, 0)",
+// 	"rgb(0, 255, 0)",
+// 	"rgb(0, 255, 255)",
+// 	"rgb(0, 0, 255)",
+// 	"rgb(255, 0, 255)",
+// ]
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.getElementById("message");
+var h1 = document.querySelector("h1");
+
 colorDisplay.textContent=pickedColor;
+
+
+
 
 for (var i=0; i< squares.length; i++){
 	squares[i].style.backgroundColor = colors[i] 
@@ -21,9 +29,40 @@ for (var i=0; i< squares.length; i++){
 		var clickedColor = this.style.backgroundColor;
 
 		if (clickedColor === pickedColor) {
-			alert("Correct");
+			h1.style.backgroundColor = pickedColor;
+			messageDisplay.textContent = "Correct";
+			changeColors(pickedColor);
 		} else {
 			this.style.backgroundColor = "#232323"
+			messageDisplay.textContent = 'Try Again'
 		}
 	})
+}
+
+function changeColors (color) {
+	for (var i = 0; i < squares.length; i++){
+		squares[i].style.backgroundColor = color
+	}
+}
+
+function pickColor(){
+	var random = Math.floor(Math.random()*colors.length)
+	return colors[random]
+}
+
+function generateRandomColors(num){
+	// make an array
+	var arr = []
+	//add num random colors to array
+	for(var i=0; i<num; i++){
+		arr.push(randomColor())
+	}
+	return arr
+}
+
+function randomColor(){
+	var r = Math.floor(Math.random()*256)
+	var g = Math.floor(Math.random()*256)
+	var b = Math.floor(Math.random()*256)
+	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
